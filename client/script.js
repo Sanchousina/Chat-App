316b93161cf072b487f7f46300b0ac3b33a0bd8d
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2023-12-22 11:40:08 +0100

    Move top-level code into `main` function

--- client/script.js
+++ client/script.js
@@ -4,26 +4,13 @@ const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 const messageContainer = document.getElementById("message-container");
 
-let username = prompt('What is your name?');
-
-if (username) {
-  appendMessage('You joined to the chat');
-  socket.emit('new-user', username);
+function appendMessage(msg) {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = msg;
+  messageContainer.append(messageElement);
 }
 
-socket.on('new-user-joined', (name) => {
-  appendMessage(`${name} joined`);
-})
-
-socket.on('chat-message', (data) => {
-  appendMessage(`${data.username}: ${data.message}`);
-})
-
-socket.on('user-disconnected', (username) => {
-  appendMessage(`${username} left chat`);
-})
-
-messageForm.addEventListener('submit', (e) => {
+function submitMessage(e) {
   e.preventDefault();
 
   const message = messageInput.value;
@@ -33,10 +20,34 @@ messageForm.addEventListener('submit', (e) => {
   socket.emit('send-chat-message', message);
 
   messageInput.value = '';
-})
-
-function appendMessage(msg) {
-  const messageElement = document.createElement('div');
-  messageElement.innerText = msg;
-  messageContainer.append(messageElement);
 }
+
+function main() {
+  const username = prompt('What is your name?');
+
+  if (username) {
+    appendMessage('You joined to the chat');
+    console.log(username);
+    socket.emit('new-user', username);
+
+    messageForm.addEventListener('submit', (e) => submitMessage(e));
+
+    socket.on('new-user-joined', (name) => {
+      appendMessage(`${name} joined`);
+    })
+    
+    socket.on('chat-message', (data) => {
+      appendMessage(`${data.username}: ${data.message}`);
+    })
+    
+    socket.on('user-disconnected', (username) => {
+      appendMessage(`${username} left chat`);
+    })
+  } else {
+    messageForm.addEventListener('submit', () => {
+      alert('Enter your name to join chat');
+    });
+  }
+}
+
+main();
